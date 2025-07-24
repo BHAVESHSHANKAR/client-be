f532ca748789e23e11033455951941813aaa1353
BHAVESH <kallurbhavesh@gmail.com>
2025-07-24 11:53:20 +0530

    changed version and packages and sh

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-pip install --upgrade pip setuptools wheel
+set -e
+
+python --version
+pip install --upgrade "pip>=23.1" "setuptools>=68.0.0" "wheel>=0.41.2"
+# Turn off PEP 517 isolated builds so pip doesn't pull an ancient setuptools for sdists
+export PIP_NO_BUILD_ISOLATION=1
 pip install -r requirements.txt
